fc1a6bc3ff023af7487752a062a172d9dcb91706
emehdii <mehdi.mahdaoui42@gmial.com.com>
2023-07-18 15:59:31 +0100

    0x02,c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char i;
+
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+
+	putchar('\n');
+}
