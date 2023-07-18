f474bf0ac99516550e3b79c3e5200deb1e49b2dc
emehdii <mehdi.mahdaoui42@gmial.com.com>
2023-07-18 20:50:54 +0100

    0x02

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,11 +6,11 @@
  * @n: the int to check
  * Return: the absolute value of int
  */
-int _abs(int i)
+int _abs(int n)
 {
-	if (i >= 0)
+	if (n >= 0)
 	{
-		return (i);
+		return (n);
 	}
-	return (-i);
+	return (-n);
 }
