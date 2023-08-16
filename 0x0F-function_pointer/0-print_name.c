86b363eb9203494e95e75a3328d3b6f0186f0231
emehdii <mehdi.mahdaoui42@gmail.com>
2023-08-16 16:24:58 +0100

    0x0F,c

--- 0x0F-function_pointer/0-print_name.c
+++ 0x0F-function_pointer/0-print_name.c
@@ -1,9 +1,11 @@
+#include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: function pointer
- * Return: void
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
--- 0x0F-function_pointer/1-array_iterator.c
+++ 0x0F-function_pointer/1-array_iterator.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter
- * on each element of an array
+ * array_iterator - executes a function given as a
+ * parameter on each element of an array.
+ * @array: input integer array.
+ * @size: size of the array.
+ * @action: pointer to the function.
  *
- * @array: array
- * @size: size of array
- * @action: pointer to function
- * Return: void
+ * Return: no return.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (!array || !action)
-		return;
+	unsigned int i;
 
-	while (size--)
-		action(*array++);
+	if (array && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
